d1dfea05d216295fb5103ee6a30ffeb23ebc7e60
puke <zijiao.wzj@alibaba-inc.com>
2025-07-02 16:57:16 +0800

    添加参数约束

--- mcp-server/tools/v_inject_audio_by_mmaudio.py
+++ mcp-server/tools/v_inject_audio_by_mmaudio.py
@@ -5,8 +5,8 @@ from utils.comfyui_util import execute_workflow
 @mcp_tool
 async def v_inject_audio_by_mmaudio(
     video: str = Field(description="The video to inject audio, must be a url"),
-    prompt: str = Field("", description="The positive prompt to generate audio"),
-    negative_prompt: str = Field("", description="The negative prompt to generate audio"),
+    prompt: str = Field("", description="The positive prompt to generate audio, must be english"),
+    negative_prompt: str = Field("", description="The negative prompt to generate audio, must be english"),
 ):
     """
     Generate audio based on video using MMAudio, and inject audio into video.
