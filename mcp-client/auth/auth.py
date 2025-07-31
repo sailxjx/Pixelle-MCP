3fd1a7a1da32c91f491b801f2e93a0776854d40d
puke <zijiao.wzj@alibaba-inc.com>
2025-07-31 16:17:35 +0800

    支持配置CHAINLIT_AUTH_ENABLED

--- mcp-client/auth/auth.py
+++ mcp-client/auth/auth.py
@@ -1,13 +1,17 @@
 # Copyright (C) 2025 AIDC-AI
 # This project is licensed under the MIT License (SPDX-License-identifier: MIT).
 
+import os
 import chainlit as cl
 
-@cl.password_auth_callback
-def auth_callback(username: str, password: str):
-    if (username, password) == ("dev", "dev"):
-        return cl.User(
-            identifier="dev", metadata={"role": "user", "provider": "credentials"}
-        )
-    else:
-        return None
+chainlit_auth_enabled = os.getenv("CHAINLIT_AUTH_ENABLED", "false").lower() == "true"
+
+if chainlit_auth_enabled:
+    @cl.password_auth_callback
+    def auth_callback(username: str, password: str):
+        if (username, password) == ("dev", "dev"):
+            return cl.User(
+                identifier="dev", metadata={"role": "user", "provider": "credentials"}
+            )
+        else:
+            return None
